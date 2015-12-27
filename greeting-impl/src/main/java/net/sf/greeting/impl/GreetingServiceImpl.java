2a1b9f3414feee9265fd97a0211d9e2908f2e1d4
Antonio Maria Sanchez Berrocal <antoniomariasanchez@gmail.com>
2015-12-27 22:09:41 +0200

    Added karaf feature

--- greeting-impl/src/main/java/net/sf/greeting/impl/GreetingServiceImpl.java
+++ greeting-impl/src/main/java/net/sf/greeting/impl/GreetingServiceImpl.java
@@ -1,7 +1,9 @@
 package net.sf.greeting.impl;
 
+import aQute.bnd.annotation.component.Component;
 import net.sf.greeting.api.GreetingService;
 
+@Component
 public class GreetingServiceImpl implements GreetingService{
 
 	public String greet(String name) {
